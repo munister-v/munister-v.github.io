ef3c191b5d8abb45abf4b620796fe98f0cb0247c
Vyacheslaw Munister <munister@outlook.com>
2026-09-18 11:49:42 +0300

    modeler: bump release version (Cloudflare had cached a 404 for sandbox.js from a too-early check right after the previous deploy)

--- modeler/js/model.js
+++ modeler/js/model.js
@@ -1,5 +1,5 @@
 // Модель данных + история (undo/redo) + автосохранение
-import { t } from './i18n.js?v=202609181147';
+import { t } from './i18n.js?v=202609181149';
 
 let seq = Date.now();
 export const uid = (p = 'id') => `${p}${(seq++).toString(36)}`;
--- modeler/js/panel.js
+++ modeler/js/panel.js
@@ -1,8 +1,8 @@
 // Properties panel: model, table (columns, keys, indexes) or foreign key
-import { ORACLE_TYPES, TABLE_COLORS, newColumn, uid, uniqueName } from './model.js?v=202609181147';
-import { sequenceDDL } from './ddl-gen.js?v=202609181147';
-import { t, getLang, onLang } from './i18n.js?v=202609181147';
-import { COLUMN_PRESETS } from './templates.js?v=202609181147';
+import { ORACLE_TYPES, TABLE_COLORS, newColumn, uid, uniqueName } from './model.js?v=202609181149';
+import { sequenceDDL } from './ddl-gen.js?v=202609181149';
+import { t, getLang, onLang } from './i18n.js?v=202609181149';
+import { COLUMN_PRESETS } from './templates.js?v=202609181149';
 
 const esc = s => String(s ?? '').replace(/[&<>"]/g, ch => ({ '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;' }[ch]));
 const CHECK_TEMPLATES = [
